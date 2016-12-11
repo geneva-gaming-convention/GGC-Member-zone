ad0b7453d93ed7372646ec04800e94160b8fbab2
Clément Hampaï <clement.hampai@cypressxt.net>
2016-12-11 19:52:30 +0100

    Solved user's creation bug
    (The new user was not logged after creation)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,42 +1,42 @@
 class UsersController < ApplicationController
-    before_action :must_be_proprietary, only: [:edit, :update]
+  before_action :must_be_proprietary, only: [:edit, :update]
 
-    def new
-        @user = User.new()
+  def new
+    @user = User.new()
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.gen_token_and_salt
+    if @user.save
+      log_in_session(@user)
+      redirect_to user_path(@user.id)
+    else
+      flash.now[:danger] =  "Failed to create "+@user.name + ", check every fields."
+      render 'new'
     end
+  end
 
-    def create
-        @user = User.new(user_params)
-        @user.gen_token_and_salt
-        if @user.save
-            flash.now[:success] = @user.name + " created successfully !"
-            redirect_to user_path(@user.id)
-        else
-            flash.now[:danger] =  "Failed to create "+@user.name + ", check every fields."
-            render 'new'
-        end
+  def show
+    @user = User.find_by(id: params["id"])
+    if !@user
+      render_404
     end
+  end
 
-    def show
-        @user = User.find_by(id: params["id"])
-        if !@user
-            render_404
-        end
+  def edit
+    @user = current_logged_user
+    @address = Address.new()
+    if !@user
+      render_404
     end
+  end
 
-    def edit
-        @user = current_logged_user
-        @address = Address.new()
-        if !@user
-            render_404
-        end
-    end
+  def update
 
-    def update
+  end
 
-    end
-
-    def user_params
-        params.require(:user).permit(:name, :lastname, :mail, :password, :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :lastname, :mail, :password, :password_confirmation)
+  end
 end
