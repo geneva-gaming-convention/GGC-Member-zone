f75dc05f637ec6b9a7653c0972e4a82513b43185
Clément Hampaï <clement.hampai@cypressxt.net>
2017-01-02 08:18:04 +0100

    display lastest 5 updates

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
   def index
     github = Github.new login:Rails.application.secrets.github_user, password:Rails.application.secrets.github_password
     @commits=[]
-    data = github.repos.commits.all 'geneva-gaming-convention', 'GGC-Website', per_page: 10
+    data = github.repos.commits.all 'geneva-gaming-convention', 'GGC-Website', per_page: 5
     data.each do |commit|
       @commits << commit.commit
     end
