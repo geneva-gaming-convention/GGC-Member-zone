6c436dac97de20b000c54ea7dec14e831f4ef4d8
Clément Hampaï <clement.hampai@cypressxt.net>
2017-01-08 00:49:09 +0100

    Update to support new url
    , https://preprod.ggc.ch

--- config/environments/preproduction.rb
+++ config/environments/preproduction.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'https://ggc-host.cypressxt.net' }
+  config.action_mailer.default_url_options = { :host => 'https://preprod.ggc.ch' }
 
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
@@ -95,8 +95,8 @@ Rails.application.configure do
   #    :ssl => true
   # }
 
-  config.action_controller.asset_host = 'https://ggc-host.cypressxt.net'
-  config.action_mailer.asset_host = 'https://ggc-host.cypressxt.net'
+  config.action_controller.asset_host = 'https://preprod.ggc.ch'
+  config.action_mailer.asset_host = 'https://preprod.ggc.ch'
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
