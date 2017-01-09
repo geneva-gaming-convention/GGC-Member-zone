03c148c4d1a7c913c395eaf5c1fe8ace5011a498
Clément Hampaï <clement.hampai@cypressxt.net>
2017-01-09 19:15:57 +0100

    Update modal & prepare for Ajax get commits

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,4 +9,14 @@ class WelcomeController < ApplicationController
     end
   end
 
+  def get_commit
+    github = Github.new login:Rails.application.secrets.github_user, password:Rails.application.secrets.github_password
+    @commits=[]
+    data = github.repos.commits.all 'geneva-gaming-convention', 'GGC-Website', per_page: 5
+    data.each do |commit|
+      @commits << commit.commit
+    end
+    return @commits
+  end
+
 end
