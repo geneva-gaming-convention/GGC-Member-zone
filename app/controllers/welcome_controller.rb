31fd8160ba5253acc1d0b61c438e9e14134bb1ba
Clément Hampaï <clement.hampai@cypressxt.net>
2017-03-02 20:56:16 +0100

    Show env info on the top menu

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,7 +6,16 @@ class WelcomeController < ApplicationController
   def get_commits
     github = Github.new login:Rails.application.secrets.github_user, password:Rails.application.secrets.github_password
     @commits=[]
-    data = github.repos.commits.all 'geneva-gaming-convention', 'GGC-Website', per_page: 5
+    data=[]
+    branches = github.repos.branches 'geneva-gaming-convention', 'GGC-Website' do |branch|
+      if Rails.application.secrets.github_branch && branch.name == Rails.application.secrets.github_branch
+        @branch_title = branch.name.capitalize
+        data = github.repos.commits.list 'geneva-gaming-convention', 'GGC-Website', :sha => branch.name, per_page: 5
+      else
+        @branch_title = ""
+        data = github.repos.commits.all 'geneva-gaming-convention', 'GGC-Website', per_page: 5
+      end
+    end
     data.each do |commit|
       @commits << commit.commit
     end
