b3bd7ef318e3e5f9fb29d0ee99f638c4e2245068
Clément Hampaï <clement.hampai@cypressxt.net>
2017-03-04 18:52:04 +0100

    Updated the db seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # Create GGC Events
-ggcOT = Event.first_or_create("name"=>"GGC Online Tournanent", "shortname"=>"GGC Online Tournanent", "description"=>"Online ESports Tournanent", "date"=>"2017-09-21 09:00:00")
+ggcOT = Event.first_or_create("name"=>"GGC Online Tournament", "shortname"=>"GGC Online Tournament", "description"=>"Online ESports Tournament", "date"=>"2017-04-08 20:00:00")
 event_resource = EventResource.first_or_create("title"=>"GGC Online Tournament: Overwatch", "description"=>"Team up and join the battle to win a free entry for you and your team at the the Geneva Gaming Convention 2017 Overwatch tournament.", "start_at"=>"2017-04-08 20:00:00", "remote"=>true, "remote_url"=>"https://widget.toornament.com/tournaments/589d946a140ba044458b461c/","event_id"=> ggcOT.id)
 # EventResource.first_or_create("title"=>"", "description"=>"", "start_at"=>"", "remote"=>true, "remote_url"=>"","event_id"=> ggcOT.id)
 # EventResource.first_or_create("title"=>"", "description"=>"", "start_at"=>"", "remote"=>true, "remote_url"=>"","event_id"=> ggcOT.id)
