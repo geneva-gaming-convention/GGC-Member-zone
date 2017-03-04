6fbb0d670b3181904b87aa589caa670d462c49fb
Clément Hampaï <clement.hampai@cypressxt.net>
2017-03-04 15:31:47 +0100

    Update preproduction env settings

--- config/deploy/preproduction.rb
+++ config/deploy/preproduction.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server 'ggc-host.cypressxt.net', user: 'cypress', roles: %w{app db web}, port: 22108
+server 'ggc-preprod.cypressxt.net', user: 'cypress', roles: %w{app db web}, port: 22108
 
 # role-based syntax
 # ==================
