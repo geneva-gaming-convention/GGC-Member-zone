db64582fdee20e109f254f9ad802521cfd9762f8
Clément Hampaï <clement.hampai@cypressxt.net>
2017-07-14 19:59:43 +0200

    Fixed migration

--- db/migrate/20170714134530_update_registration_model.rb
+++ db/migrate/20170714134530_update_registration_model.rb
@@ -1,13 +1,9 @@
 class UpdateRegistrationModel < ActiveRecord::Migration[5.0]
   def change
-    add_column :registrations, :event_resource_id, :integer
-    add_foreign_key :event_resource, :registrations
-    add_column :registrations, :event_pack_id, :integer
-    add_foreign_key :event_pack, :registrations
-    add_column :registrations, :team_id, :integer
-    add_foreign_key :team, :registrations
-    add_column :registrations, :users_group_id, :integer
-    add_foreign_key :users_group, :registrations
+    add_reference :registrations, :event_resource, index: true
+    add_reference :registrations, :event_pack, index: true
+    add_reference :registrations, :team, index: true
+    add_reference :registrations, :users_group, index: true
     add_column :registrations, :is_a_player, :boolean
   end
 end
