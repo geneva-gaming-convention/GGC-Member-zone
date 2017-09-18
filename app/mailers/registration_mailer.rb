fb68d6dec8cc8a7b5e0f55996f15a7963bef469b
Clément Hampaï <clement.hampai@cypressxt.net>
2017-09-18 18:58:30 +0200

    Updated tickets

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -8,7 +8,7 @@ class RegistrationMailer < ApplicationMailer
     @registration = @user.registrations.find_by(event: @event)
     if @event && @registration && @registration.token
       @qrURL = event_qr_registration_url(@event, @registration.token)
-      @qrcode = RQRCode::QRCode.new(@qrURL)
+      @qrcode = RQRCode::QRCode.new(@qrURL, :size => 8, :level => :h)
       mail(to: @user.mail, subject: @mail_title)
     end
   end
